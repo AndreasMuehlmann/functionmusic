d78dfa4ffc6a8bd2bb983e5620c95be944b54fca
Andreas Muehlmann <andi.muehlmann@arcor.de>
2022-10-20 15:50:21 +0200

    black window + square wave

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use sdl2::video::Window;
+use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+
+mod synthesizer;
+
+
+const WINDOW_WIDTH: u32 = 1000;
+const WINDOW_HEIGHT: u32 = 1000;
+
+
 fn main() {
-    println!("Hello, world!");
+    synthesizer::play_a_sound();
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("functionmusic", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas: Canvas<Window> = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                _ => {}
+            }
+        }
+        canvas.present();
+    }
 }
