2c8474352d4117a89284936aee8bd9744f2d0afd
Andreas Muehlmann <andi.muehlmann@arcor.de>
2024-10-26 15:03:00 +0200

    functions as sounds

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,9 @@ import qualified Data.ByteString.Lazy as B
 import Data.Foldable
 import Data.List
 import Numeric (pi)
+import System.IO
 import System.Process
 import Text.Printf
-import System.IO
 
 type Pulse = Float
 
@@ -29,118 +29,85 @@ type Phase = Float
 
 type Wave = (Hz, Phase)
 
-outputFilePath :: FilePath
-outputFilePath = "output.bin"
-
-volume :: Float
-volume = 0.2
+-- freq :: Hz -> Seconds -> [Pulse]
+-- freq hz duration =
+--   map (* volume) $ zipWith3 (\x y z -> x * y * z) release attack output
+--   where
+--     step = (hz * 2 * pi) / sampleRate
+--
+--     attack :: [Pulse]
+--     attack = map (min 1.0) [0.0, 0.001 ..]
+--
+--     release :: [Pulse]
+--     release = reverse $ take (length output) attack
+--
+--     output :: [Pulse]
+--     output = map (sin . (* step)) [0.0 .. sampleRate * duration]
 
 sampleRate :: Samples
-sampleRate = 100.0
+sampleRate = 48000.0
 
 pitchStandard :: Hz
 pitchStandard = 440.0
 
-bpm :: Beats
-bpm = 120.0
-
-beatDuration :: Seconds
-beatDuration = 60.0 / bpm
+outputFilePath :: FilePath
+outputFilePath = "output.bin"
 
 -- NOTE: the formula is taken from https://pages.mtu.edu/~suits/NoteFreqCalcs.html
 semitoneToFrequency :: Semitone -> Hz
 semitoneToFrequency n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
 
-note :: Semitone -> Beats -> [Pulse]
-note n beats = freq (semitoneToFrequency n) (beats * beatDuration)
-
-freq :: Hz -> Seconds -> [Pulse]
-freq hz duration =
-  map (* volume) $ zipWith3 (\x y z -> x * y * z) release attack output
-  where
-    step = (hz * 2 * pi) / sampleRate
-
-    attack :: [Pulse]
-    attack = map (min 1.0) [0.0, 0.001 ..]
-
-    release :: [Pulse]
-    release = reverse $ take (length output) attack
-
-    output :: [Pulse]
-    output = map (sin . (* step)) [0.0 .. sampleRate * duration]
-
-wave :: [Pulse]
-wave =
-  concat
-    [ note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.5,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.5,
-      note 5 0.25,
-      note 5 0.25,
-      note 5 0.25,
-      note 5 0.25,
-      note 5 0.25,
-      note 5 0.25,
-      note 5 0.5,
-      note 3 0.25,
-      note 3 0.25,
-      note 3 0.25,
-      note 3 0.25,
-      note 3 0.25,
-      note 3 0.25,
-      note 3 0.5,
-      note (-2) 0.5,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.25,
-      note 0 0.5
-    ]
 phaseToMatchPreviousSinWave :: Wave -> Float -> Hz -> Phase
 phaseToMatchPreviousSinWave (prevFreq, prevPhase) x freq = prevFreq * 2 * pi * x + prevPhase - (freq * 2 * pi * x)
 
-sampleRateFunctions :: Float
-sampleRateFunctions = sampleRate
-
-nextPulse :: Wave -> Float -> Hz -> (Pulse, Wave)
-nextPulse wave x freq = (sin (freq * 2 * pi * nextX + phase), (freq, phase))
+nextPulse :: Wave -> Float -> Hz -> (Pulse, Wave, Float)
+nextPulse wave x freq = (sin (freq * 2 * pi * (x - step) + phase), (freq, phase), x + step)
   where
-    nextX = x + 1 / sampleRateFunctions
+    step = 1 / sampleRate
     phase = phaseToMatchPreviousSinWave wave x freq
 
 toPulses :: [Semitone] -> Wave -> Float -> [Pulse]
 toPulses [] wave prevX = []
-toPulses (semitone : semitones) prevWave prevX = pulse : toPulses semitones wave (prevX + 1 / sampleRateFunctions)
-  where (pulse, wave) = nextPulse prevWave prevX (semitoneToFrequency semitone)
+toPulses (semitone : semitones) prevWave prevX = pulse : toPulses semitones wave nextX
+  where
+    (pulse, wave, nextX) = nextPulse prevWave prevX (semitoneToFrequency semitone)
 
-functionAsSemitones :: SoundFunction -> Float -> Float -> [Pulse]
-functionAsSemitones soundFunction from duration = toPulses semitones (pitchStandard, 0.0) 0.0
+functionAsPulses :: SoundFunction -> Float -> Float -> [Pulse]
+functionAsPulses soundFunction from duration = toPulses semitones (pitchStandard, 0.0) 0.0
   where
     semitones = map (soundFunction . (/ sampleRate)) [0.0 .. sampleRate * duration]
-
-save :: FilePath -> IO ()
-save filePath = B.writeFile filePath $ BB.toLazyByteString $ foldMap BB.floatLE $ functionAsSemitones parabel 0 0.1
 
 constant :: Float -> SoundFunction
 constant value x = value
 
-parabel :: SoundFunction
-parabel x = x * x - 100
+linear :: Float -> Float -> SoundFunction
+linear sloap yIntercept x = sloap * x + yIntercept
+
+parabel :: Float -> Float -> SoundFunction
+parabel koefficient yIntercept x= koefficient * x * x + yIntercept
 
 
-saveRawFloats :: FilePath -> IO ()
-saveRawFloats filePath = do
-    let contents = unlines $ map show $ functionAsSemitones parabel 0 5
-    writeFile filePath contents
+volume :: Float
+volume = 0.2
+
+soundFunction :: SoundFunction
+soundFunction = constant (-10)
+
+duration :: Float
+duration = 10.0
+
+start :: Float
+start = 0.0
+
+saveAsCsv :: FilePath -> IO ()
+saveAsCsv filePath = do
+  let contents = unlines $ zipWith (++) (map (++ ",") xs) pulses
+  writeFile filePath contents
+  where pulses = map show $ functionAsPulses soundFunction start duration
+        xs     = map (show . (/ sampleRate)) [0.0 .. fromIntegral (length pulses)]
+
+save :: FilePath -> IO ()
+save filePath = B.writeFile filePath $ BB.toLazyByteString $ foldMap (BB.floatLE . (volume *)) (functionAsPulses soundFunction start duration)
 
 play :: IO ()
 play = do
